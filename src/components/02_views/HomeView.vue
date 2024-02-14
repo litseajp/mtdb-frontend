6e0bd9f881b9721efe2715f7bf6a33a4842f175c
Litsea <litsea52@gmail.com>
2024-02-14 20:52:25 +0900

    add: トップページにツール一覧を追加

--- src/components/02_views/HomeView.vue
+++ src/components/02_views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+  import ToolList from '@/components/03_sections/home/ToolList.vue'
 </script>
 
 <template>
+  <h1>ツール一覧</h1>
+  <ToolList />
 </template>
 
 <style scoped>
---
+++ src/components/03_sections/home/ToolList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import ToolListItem from '@/components/04_parts/home/ToolListItem.vue'
+  import toolList from '@/constants/toolList'
+</script>
+
+<template>
+  <section id="tool-list" class="mt-12">
+    <div id="tool-list-container">
+      <ToolListItem v-for="tool in toolList" :key="tool.path" :tool="tool" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  #tool-list-container {
+    display: flex;
+    justify-content: center;
+  }
+</style>
