91e6c2d511f3201f1c156b2a09a4a656a98add69
Litsea <litsea52@gmail.com>
2024-02-14 23:00:43 +0900

    add: コード詳細ページに鍵盤図を追加

--- src/components/02_views/ChordView.vue
+++ src/components/02_views/ChordView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-  import { onMounted, provide, ref } from 'vue'
+  import { computed, onMounted, provide, ref } from 'vue'
   import { useRoute } from 'vue-router'
   import LoadingMessage from '@/components/03_sections/shared/LoadingMessage.vue'
   import PageDescription from '@/components/03_sections/shared/PageDescription.vue'
   import ChordToneList from '@/components/03_sections/chord/ChordToneList.vue'
+  import KeyboardDiagram from '@/components/03_sections/shared/KeyboardDiagram.vue'
   import BackwardLink from '@/components/03_sections/shared/BackwardLink.vue'
   import FetchErrorMessage from '@/components/03_sections/shared/FetchErrorMessage.vue'
   import type { ChordDetails } from '@/types/interfaces'
-  import { chordKey } from '@/types/injectionKeys'
+  import { chordKey, tonesKey } from '@/types/injectionKeys'
   import { fetchChord } from '@/utils/fetch'
 
   const route = useRoute()
@@ -37,6 +38,7 @@
   onMounted(fetchData)
 
   provide(chordKey, chord)
+  provide(tonesKey, computed(() => chord.value.tones))
 
   const linkInfo = { path: '/chords', title: 'コード一覧' }
 </script>
@@ -49,6 +51,7 @@
     <h1>{{ chord.name }}</h1>
     <PageDescription :text="chord.description" />
     <ChordToneList />
+    <KeyboardDiagram />
     <BackwardLink :linkInfo="linkInfo" />
   </template>
   <template v-else>
