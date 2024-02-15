75e46ec99b745611b63df26b6d867e3467ba413b
Litsea <litsea52@gmail.com>
2024-02-15 19:11:44 +0900

    update: トップページにレスポンシブデザインを適用

--- src/components/03_sections/home/ToolList.vue
+++ src/components/03_sections/home/ToolList.vue
@@ -4,16 +4,35 @@
 </script>
 
 <template>
-  <section id="tool-list" class="mt-14">
+  <section id="tool-list">
     <div id="tool-list-container">
-      <ToolListItem v-for="tool in toolList" :key="tool.path" :tool="tool" />
+      <ToolListItem v-for="tool in toolList" :key="tool.path" :tool="tool" class="tool-list-item" />
     </div>
   </section>
 </template>
 
 <style scoped>
+  #tool-list {
+    margin-top: 48px;
+  }
+
   #tool-list-container {
     display: flex;
     justify-content: center;
+  }
+
+  @media screen and (max-width: 767px) {
+    #tool-list {
+      margin-top: 24px;
+    }
+
+    #tool-list-container {
+      display: block;
+      width: 100%;
+    }
+
+    .tool-list-item:not(:first-child) {
+      margin-top: 28px;
+    }
   }
 </style>
